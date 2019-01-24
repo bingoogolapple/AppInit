a87e5f63fc2d53d0c0b574c9e2d7c9e56d5a378e
bingoogolapple <bingoogolapple@gmail.com>
2019-01-25 00:14:58 +0800

    v1.0.1 优化 BaseTransform

--- updateVersion.sh
+++ updateVersion.sh
@@ -1,7 +1,7 @@
 #!/bin/bash +x
 
-OLD_VERSION="0.0.1"
-NEW_VERSION="1.0.0"
+OLD_VERSION="1.0.0"
+NEW_VERSION="1.0.1"
 
 sed -i -e "s/POM_VERSION_NAME=$OLD_VERSION/POM_VERSION_NAME=$NEW_VERSION/" gradle.properties
 sed -i -e "s/VERSION_NAME = \"$OLD_VERSION\"/VERSION_NAME = \"$NEW_VERSION\"/" buildSrc/src/main/groovy/com/sankuai/erp/component/plugin/appinit/AppInitPlugin.groovy
